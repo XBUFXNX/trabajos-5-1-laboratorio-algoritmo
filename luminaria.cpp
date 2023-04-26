713964415cf877898c9d0b5939c23db64ed406c7
XBUFXNX <130572327+XBUFXNX@users.noreply.github.com>
2023-04-26 09:54:16 -0300

    Add files via upload

---
+++ luminaria.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int  max_luz[5]
+float 
+main()
+{
+	for(i = 0; i < 5; i++) //pide el maximo de luz por calle
+	{
+		printf ("ingrese el numero de luces maximos que se podran encender en la calle N° %d", i);
+		scanf ("%d", &max_luz[i]);
+	}
+	
+	do{
+		printf("ingrese el numero de calle y el numero de luz"); //pide el que luces y en que calle quiere encender.
+		scanf("%d %d", matriz[calle][luz]);
+		{
+			
+		}
+		
+		
+	}while()//luz == 0 termina el ciclo
+	
+	
+	
+	
+	/////////////////////////////////
+	
+	sumar todo lo de una calle y tiene que dar 0 = oscuridad
+	si hay luces = a maximo || luz encendidas es > apagadas = luminosa
+	
+	do{
+	printf("ingrese la cantidad de luces que estaran encendidas en la calle %d", i);
+	scanf("%d", &luz)
+	
+	if(max_luz[i] >= luz)
+	{
+		max_luz[i]= max_luz[i + 1];
+	}
+	
+	
+	}while(luz != 0)
+	
+}
+
+printf("ingrese el numero de calle y el numero de luz");
+scanf("%d, %d", i j)
+matriz [i][j]
+
